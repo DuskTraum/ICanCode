e81badc1cd95994edfcf618a03be5cf5da7f9ec5
Yuri_Karpov <Yuri_Karpov@EPRURYAW0064.moscow.epam.com>
2016-08-29 15:15:06 +0300

    Delete TO-DO

--- ICanCode/src/test/java/com/epam/yuri_karpov/icancode/GameTest.java
+++ ICanCode/src/test/java/com/epam/yuri_karpov/icancode/GameTest.java
@@ -78,7 +78,6 @@ public class GameTest {
 			Thread.sleep(5000);
 		}
 		catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		LOG.info("finish 'prepareGame'");
